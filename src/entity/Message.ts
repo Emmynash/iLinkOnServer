5c82ed8fbf411400563e55324eed4f8b6d34762d
Akita Emmanuel <e.akita91@gmail.com>
2020-10-25 20:12:38 +0100

    Add image, audio, file and messageType

--- src/entity/Message.ts
+++ src/entity/Message.ts
@@ -5,6 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { IsOptional } from 'class-validator';
 import { BaseEntity } from './BaseEntity';
 import { User } from './User';
 import { MessageThread } from './MessageThread';
@@ -15,10 +16,30 @@ export enum MessageStatus {
   SENT = 'sent',
 }
 
+export enum MessageType {
+  AUDIO = 'audio',
+  FILE = 'file',
+  IMAGE = 'image',
+  TEXT = 'text',
+}
+
 @Entity()
 export class Message extends BaseEntity {
-  @Column()
+  @Column({ default: null })
+  @IsOptional()
   public text: string;
+
+  @Column({ default: null })
+  @IsOptional()
+  public audio: string;
+
+  @Column({ default: null })
+  @IsOptional()
+  public image: string;
+
+  @Column({ default: null })
+  @IsOptional()
+  public file: string;
 
   @ManyToOne((type) => User, (user) => user.sentMessages, {
     eager: true,
@@ -34,6 +55,13 @@ export class Message extends BaseEntity {
     default: MessageStatus.SENT,
   })
   public status: MessageStatus;
+
+  @Column({
+    type: 'enum',
+    enum: MessageType,
+    default: MessageType.TEXT,
+  })
+  public messageType: MessageType;
 
   @CreateDateColumn()
   public createdAt: Date;
